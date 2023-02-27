7436e6786b247890e9ab04966d7aa006b9452940
kingdreamerr <jackmvula458@gmail.com>
2023-02-27 23:25:40 +0200

    Set up game logic

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ screen.title("USA States Game")
 img = 'blank_states_img.gif'
 screen.addshape(img)
 turtle.shape(img)
+states_guessed = []
 
 data = pandas.read_csv('50_states.csv')
 
-print(data)
+
+while len(states_guessed) < 50:
+    answer = screen.textinput(title=f"{len(states_guessed)}/50 states correct",
+                           prompt="what's another state's name?").title()
 screen.exitonclick()
