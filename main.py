59ef95716afc63dce82792f3818a4c35ad6e6036
kingdreamerr <jackmvula458@gmail.com>
2023-02-27 23:27:33 +0200

    Appened the user answer to the guess list

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ turtle.shape(img)
 states_guessed = []
 
 data = pandas.read_csv('50_states.csv')
+states_list = data['state'].to_list()
 
 
 while len(states_guessed) < 50:
     answer = screen.textinput(title=f"{len(states_guessed)}/50 states correct",
                            prompt="what's another state's name?").title()
+    
+    if answer in states_list:
+        states_guessed.append(answer)
+        
 screen.exitonclick()
